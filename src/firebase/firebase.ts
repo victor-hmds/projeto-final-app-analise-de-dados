28417b1ccb9934ef3850418b32253bbd47cef309
victor-hmds <victormartins.souza@gmail.com>
2025-02-14 22:14:37 -0300

    add arquivos

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, MEASUREMENT_ID } from '@env';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOAvkK_w_FmdKGfCtiZ-tDOt6oPCF_I40",
-  authDomain: "autenticador-ad946.firebaseapp.com",
-  projectId: "autenticador-ad946",
-  storageBucket: "autenticador-ad946.firebasestorage.app",
-  messagingSenderId: "85270748438",
-  appId: "1:85270748438:web:b1c93d795821310e268437",
-  measurementId: "G-Q71L8HBVZL"
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
